64c86f9d776c19435833c605405336f29609a769
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-30 23:39:01 +0600

    router-simple project setup done

---
+++ src/componants/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+const Home = () => {
+    return (
+        <div>
+            <ul className="flex justify-center space-x-6 text-lg rounded-lg shadow-md py-6 my-4  font-semibold"  >
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Home;

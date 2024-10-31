0b2598270a279a195e235fc79ead40bef767d12d
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-31 13:19:41 +0600

    navigation done

--- src/componants/Home/Home.jsx
+++ src/componants/Home/Home.jsx
@@ -1,18 +1,18 @@
 import { Outlet, useNavigation } from "react-router-dom";
-import Header from './../Header/Header';
+import Header from "./../Header/Header";
 const Home = () => {
-    // for loading 
-    const navigation = useNavigation();
-    return (
-        <div>
-            <Header></Header>
-            {
-                navigation.state === "loading"?<span className="loading loading-dots loading-lg"></span>
-                :<Outlet></Outlet>
-            }
-            
-        </div>
-    );
+  // for loading
+  const navigation = useNavigation();
+  return (
+    <div>
+      <Header></Header>
+      {navigation.state === "loading" ? (
+        <span className="loading loading-bars loading-lg"></span>
+      ) : (
+        <Outlet></Outlet>
+      )}
+    </div>
+  );
 };
 
 export default Home;
